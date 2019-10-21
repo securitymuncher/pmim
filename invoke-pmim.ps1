aef9dde2e1e5e248c81c15de84bb9d90b8edc646
thenamol <53873876+thenamol@users.noreply.github.com>
2019-10-21 16:58:36 -0400

    Update invoke-pmim.ps1

--- invoke-pmim.ps1
+++ invoke-pmim.ps1
@@ -4,19 +4,27 @@
     This script will find users within the active directory environement with stale accounts.
 
 .DESCRIPTION
-    <fill this in later dumbo>
+    Powershell script to search a given OU for stale users based on the script input.
 
 .INPUTS fileOutPath
-    Path where you want to store the output files.
+    Path where you want to store the output files of the script.
 
 .INPUTS moveusers
-    Confirmation switch that you want to move the users. If you want to move them, use -moveusers $true
+    Confirmation switch that you want to move the users. If you want to move the users, include the -MoveUsers parameter.
+
+.INPUTS disableUsers
+    Confirmation switch that you want to disable the stale users found. If you want to disable the users, include the -disableUsers parameter.
+
+.INPUTS daysStale
+    This tells the script how many days you want to use as a basis to determine if an account is stale.
 
 .INPUTS domainCredentials
+    ***Not Implemented***
     User with domain credentials to perform administrative actions. If this is not passed by default and the -Batch flag is not passed as well,
-    the script will prompt the user to enter domain credentials.
+    the script will prompt the user to enter domain credentials. 
 
 .INPUTS batch
+    ***Not Implemented***
     This flag sets the script to run in a batch mode, not prompting for credentials. This will assume the scheduled task or process calling it
     has the appropiate permissions in active directory and on the machine where it is ran.
 
@@ -24,7 +32,7 @@
     <What outputs will occur? Log file gots to be stored somewhere!>
 
 .NOTES
-    Version: 1.0
+    Version: 1.1
     Author: Patrick Lowther (PL)
     Creation Date: 2019-09-04
     Purpose: Powershell script to return a list of all stale user accounts in the Active Directory Environment, move those users to a target
@@ -36,38 +44,34 @@
 #>
 param(
     [cmdletbinding()]
-        [Parameter(Mandatory = $true, HelpMessage ="Path to a directory to write log files and CSVs out to.")]
+        [Parameter(Mandatory = $true, HelpMessage = "Path to a directory to write log files and CSVs out to.")]
         [ValidateScript({Test-Path $_ -PathType Container})]
         [string]$fileOutPath,
 
+        #Disable the below parameters until we flesh this all the way out
+        # [Parameter(ParameterSetName="BatchSettings",Mandatory=$false)][switch]$batch = $false,
+        # [Parameter(ParameterSetName="BatchSettings",Mandatory=$true)][string]$batchFile
+
+        [Parameter(Mandatory = $true, HelpMessage = "The full distinguished name of the OU you want to search")]
+        [string]$searchOU,
+
+        [Parameter(Mandatory = $true, HelpMessage = "The full distinguished name of the Stale User OU")]
+        [string]$staleUserOU,
+        
+        [Parameter(Mandatory = $true, HelpMessage = "The number of days to consider an account stale.")]
+        [int32]$daysStales,
+        
+        [Parameter(Mandatory = $false, HelpMessage = "True or False. Setting this to true will move the users to the specified Stale User OU")]
         [allowNull()]
         [switch]$moveUsers=$false,
 
+        [Parameter(Mandatory = $false, HelpMessage = "True or False. Setting this to true will disable the users.")]
         [allowNull()]
-        [switch]$batch = $false
-
+        [switch]$disableUsers=$false
         # [allowNull()]
         # [string]$domainCredentials = (Get-Credential -message "Please enter a user with Active Directory domain administration privalleges")
 
 )
-
-#---User Defined Variables---#
-
-# This is a multidimensional array with the first element being the organizational unit you want to set as your search base. The second element is the number of days stale. This
-# is expecting the OU in the fully distinguished name format.
-
-$searchBaseOUs = @("OU=Example1,DC=CONTOSO,DC=COM",30), @("OU=Example2,DC=CONTOSO,DC=COM",90), @("OU=CONTOSO Users,DC=CONTOSO,DC=COM",45), 
-        @("OU=Example3,DC=CONTOSO,DC=COM",30), @("OU=Example4,DC=CONTOSO,DC=COM",30), @("OU=Example5,DC=CONTOSO,DC=COM",30),
-        @("OU=Example6,DC=CONTOSO,DC=COM",30), @("OU=Example7,DC=CONTOSO,DC=COM",30)
-
-# This is the fully distinguished named for the target organizational unit where stale user accounts will be moved.
-
-$staleUserOU = "OU=Stale Accounts,DC=CONTOSO,DC=com"
-
-#---Script Initializations---#
-
-Import-Module -Name ActiveDirectory 
-
 
 #---Script Variable Declarations---#
 $timeStamp = get-date -format o | ForEach-Object {$_ -replace ":", "."}
@@ -88,14 +92,14 @@ $errorLogFile = $errorLogName + "-" + $timeStamp + $errorLogExt
 $targetSavePath = "$fileOutPath"
 $fileOutTest = "~try.01"
 $fileOutPathFile = Join-Path -Path $targetSavePath -ChildPath $fileOutTest
-$archivePath = $targetSavePath + "Archive\"
-$archiveFiles = $targetSavePath + "\*.*"
+$archivePath = Join-Path -Path $targetSavePath -ChildPath "Archive"
+$archiveFiles = Join-Path -Path $targetSavePath -ChildPath "*.*"
 $fullSavePN = Join-Path -Path $targetSavePath -ChildPath $fileName
 $logFilePN = Join-Path -Path $targetSavePath -ChildPath $logFile
 $errorLogFilePN = Join-Path -Path $targetSavePath -ChildPath $errorLogFile
 
 #Version of the Script
-$sScriptVersion = "1.0"
+$sScriptVersion = "1.1"
 $ErrorActionPreference = "silentlycontinue"
 
 #---Functions---#
@@ -136,7 +140,7 @@ function search-staletargetOU {
         [ValidateNotNullorEmpty()]
         [int32]$sTargetDays
         )
-    $sTargetOU = $sTargetOU.Replace(":",",")
+    
     $DaysStale = 0-$sTargetDays
     Search-ADAccount -AccountInactive -DateTime((get-date).adddays($DaysStale)) -UsersOnly -SearchBase "$sTargetOU"
 }
@@ -146,51 +150,57 @@ function move-staleUser {
     param (
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [string]$distinguishedName,
+
+        [parameter(Mandatory = $true)]
+        [string]$MoveToOU,
+
+        [Parameter(Mandatory = $false)]
         [allowNull()]
         [switch]$doIIIIIT = $false
     )
-    process{
+
+    process {
         foreach ($samAccount in $distinguishedName) {
-            Write-Log -Severity Info -Message "Moving $samAccount to stale users" -LogFile $logFilePN
-            write-output "Current Sam Account is $samAccount"
-            if ($doIIIIIT -eq $false) {
-                try {
-                    Write-Log -Severity Info -Message "Test Run: Moving $samAccount to stale users" -LogFile $logFilePN
-                    Write-Output "Test Run: Attempting to move $samAccount"
-                    Move-ADObject -Identity $samAccount -TargetPath $staleUserOU -ErrorVariable $moveError -WhatIf
-                    Write-Output "Test Run: Moving user would succeed!"
+                Write-Log -Severity Info -Message "Moving $samAccount to $moveToOU" -LogFile $logFilePN
+                write-output "Current Sam Account is $samAccount"
+                if ($doIIIIIT -eq $false) {
+                    try {
+                        Write-Log -Severity Info -Message "Test Run: Moving $samAccount to stale users" -LogFile $logFilePN
+                        Write-Output "Test Run: Attempting to move $samAccount"
+                        Move-ADObject -Identity $samAccount -TargetPath $moveToOU -ErrorVariable $moveError -WhatIf
+                        Write-Output "Test Run: Moving user would succeed!"
+                    }
+                    catch {
+                        Write-Output "Test Run: Failed to move $samAccount"
+                        Write-Output "$moveError"
+                        Write-Log -Severity Info -Message "Test Run: Moving $samAccount to stale users would fail" -LogFile $logFilePN
+                        Write-Log -Severity Warning -Message "Test Run: Failed to move $samAccount!" -LogFile $errorLogFilePN
+                        Write-Log -Severity Warning -Message "$moveError" -LogFile $errorLogFilePN
+                    }
                 }
-                catch {
-                    Write-Output "Test Run: Failed to move $samAccount"
-                    Write-Output "$moveError"
-                    Write-Log -Severity Info -Message "Test Run: Moving $samAccount to stale users would fail" -LogFile $logFilePN
-                    Write-Log -Severity Warning -Message "Test Run: Failed to move $samAccount!" -LogFile $errorLogFilePN
-                    Write-Log -Severity Warning -Message "$moveError" -LogFile $errorLogFilePN
-                }
-            }
-            elseif ($doIIIIIT -eq $true) {
-                try {
-                    #No training wheels on this one, it's gonna move em!
-                    Write-Log -Severity Info -Message "Moving $samAccount to stale users" -LogFile $logFilePN
-                    Move-ADObject -Identity $samAccount -TargetPath $staleUserOU -ErrorVariable $moveError
-                    Write-Log -Severity Info -Message "Successful! $samAccount moved to stale users." -LogFile $logFilePN
-                }
-                catch {
-                    Write-Log -Severity Warning -Message "Failed to move $samAccount!" -LogFile $errorLogFilePN
-                    Write-Log -Severity Warning -Message "$moveError" -LogFile $errorLogFilePN
+                elseif ($doIIIIIT -eq $true) {
+                    try {
+                        #No training wheels on this one, it's gonna move em!
+                        Write-Log -Severity Info -Message "Moving $samAccount to stale users" -LogFile $logFilePN
+                        Move-ADObject -Identity $samAccount -TargetPath $moveToOU -ErrorVariable $moveError
+                        Write-Log -Severity Info -Message "Successful! $samAccount moved to stale users." -LogFile $logFilePN
+                    }
+                    catch {
+                        Write-Log -Severity Warning -Message "Failed to move $samAccount!" -LogFile $errorLogFilePN
+                        Write-Log -Severity Warning -Message "$moveError" -LogFile $errorLogFilePN
+                    }
                 }
             }
         }
-
     }
-
-}
 
 function disable-staleUser {
     [cmdletbinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [string]$samAccountName,
+
+        [Parameter(Mandatory = $false)]
         [allowNull()]
         [switch]$doIIIIIT = $false
     )
@@ -214,6 +224,7 @@ function disable-staleUser {
                 try {
                     Write-Log -Severity Info -Message "Disabling $samAccount due to being a stale user." -LogFile $logFilePN
                     #No training wheels on this one, it's gonna disable accounts!
+                    Write-Output "Disabling $samAccount."
                     Set-ADUser -Identity $samAccount -enabled $false -ErrorVariable $disableUserError
                     Write-Log -Severity Info -Message "Success! $samAccount is now disabled. " -LogFile $logFilePN
                 }
@@ -281,8 +292,44 @@ function invoke-userCheck {
     
 }
 
+#---Script Initializations---#
+Write-Output "Initilizing script."
+Write-Output "Loading Active Directory Module"
+Import-Module -Name ActiveDirectory 
 #---Script Work---
 #try to open/write file at specified path
+
+#DEFINE VARIABLES
+$currentOU = $searchOU
+$currentDays = $daysStales
+$staleOU = $staleUserOU
+$SOUExists = [adsi]::Exists("LDAP://$staleOU")
+$TStaleOU =  [adsi]::Exists("LDAP://$currentOU")
+
+Write-Log -Message "Starting run of invoke-pmim.ps1 version $sScriptVersion" -severity Info -logFile $logFilePN
+
+try {
+    if (-not $SOUExists) {
+        Write-Log -Severity "Error" -Message "StaleOU does not exist" -LogFile $logFilePN
+        Write-Output "StaleUserOU does not exist!"
+        Exit
+    }
+}
+catch {
+    throw "Evaluation of stale target ou did not succeed."
+}
+
+try {
+    if (-not $TStaleOU) {
+        Write-Log -Severity "Error" -Message "The search OU does not exist" -LogFile $logFilePN
+        Write-Output "The SearchOU does not exist!"
+        Exit
+    }
+}
+catch {
+    throw "Evaluation of search ou did not succeed."
+}
+
 try {
     [io.file]::OpenWrite($fileOutPathFile).close()
     remove-item -Path $fileOutPathFile
@@ -304,11 +351,6 @@ catch {
 
 if (Test-Path -Path $fileOutPath -PathType Container) {
 
-Write-Log -Message "Starting run of invoke-pmim.ps1 version $sScriptVersion" -severity Info -logFile $logFilePN
-foreach ($searchBase in $searchBaseOUs) {
-    $currentOU = $searchbase[0]
-    $currentDays = $searchbase[1]
-
     Write-Output "Processing $currentOU for user accounts that are $currentDays days stale."
     Write-Log -Message "Processing $currentOU for user accounts that are $currentDays days stale." -Severity Info -LogFile $logFilePN
    
@@ -318,16 +360,26 @@ foreach ($searchBase in $searchBaseOUs) {
             $evaluatedUsers | Export-Csv -Path $fullSavePN -NoTypeInformation -Append
         }
         
-  #search-staletargetOU -sTargetOU $currentOU -sTargetDays $currentDays | move-staleUser
-    #write-Host "Searchbase: " $searchbase[0]
-    #Write-Host "Days Stale: " $searchBase[1]
-    }
+    Write-Output "move users: $moveUsers"
+    Write-Output "disable users: $disableUsers"
 $filteredUsers = Import-Csv -path $fullSavePN
     foreach ($filteredUser in $filteredUsers) {
         $currentFilteredUser = $filteredUser.sam
         $currentFilteredUserDN = $filteredUser.dn
-        disable-staleUser -samAccountName $currentFilteredUser
-        move-staleUser -distinguishedName $currentFilteredUserDN
+       
+        if ($disableUsers -eq $true) {
+            disable-staleUser -samAccountName $currentFilteredUser -doIIIIIT
+        }
+        elseif ($disableUsers -eq $false) {
+            disable-staleUser -samAccountName $currentFilteredUser
+        }
+
+        if ($moveUsers -eq $true) {
+            move-staleUser -distinguishedName $currentFilteredUserDN -MoveToOU $staleOU -doIIIIIT
+        }
+        elseif ($moveUsers -eq $false) {
+            move-staleUser -distinguishedName $currentFilteredUserDN -MoveToOU $staleOU
+        }
     }
 
 Write-Log -Message "Invoke-PMIM script has finished." -Severity Info -LogFile $logFilePN
